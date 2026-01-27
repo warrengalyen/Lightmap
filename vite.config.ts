e1d1ef192df33aac2e9157657990d490c90a0041
warrengalyen <wgalyen@hotmail.com>
2026-01-27 11:24:00 -0800

    Fix type related issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { checker } from 'vite-plugin-checker';
 import { execSync } from 'child_process';
@@ -25,9 +25,6 @@ export default defineConfig({
         },
         checker({
             typescript: true,
-            eslint: {
-                lintCommand: 'eslint .',
-            },
         }),
     ],
     define: {
